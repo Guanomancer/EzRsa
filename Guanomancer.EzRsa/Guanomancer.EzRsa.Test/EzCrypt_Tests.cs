131b98e6027cd4e815ee1ccd0051319653bc073c
Zefugi <trowi@live.dk>
2021-03-05 18:52:12 +0100

    Implemented AES wrapper

---
+++ Guanomancer.EzRsa/Guanomancer.EzRsa.Test/EzCrypt_Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guanomancer.EzRsa.Test
+{
+    [TestFixture]
+    public class EzCrypt_Tests
+    {
+        [Test]
+        public void Encrypt_CreatesOutputArray()
+        {
+            var crypt = new EzCrypt();
+            var str = "This is a test.";
+
+            var buffer = crypt.Encrypt(str);
+
+            Assert.IsNotNull(buffer);
+            Assert.AreNotEqual(0, buffer.Length);
+        }
+
+        [Test]
+        public void Decrypt_RestoresEncryptedString()
+        {
+            var crypt = new EzCrypt();
+            var str = "This is a test.";
+            var buffer = crypt.Encrypt(str);
+
+            var result = crypt.DecryptToString(buffer);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(str, result);
+        }
+    }
+}
---
+++ Guanomancer.EzRsa/Guanomancer.EzRsa/EzCrypt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Guanomancer.EzRsa
+{
+    public class EzCrypt
+    {
+        private Aes _aes;
+        private RSA _rsa;
+
+        public EzCrypt(int rsaKeyBitSize = 1024, int aesKeyBitSize = 256, int blockSize = 128)
+        {
+            if (rsaKeyBitSize % 8 != 0)
+                throw new ArgumentException("RSA bit Size must be divisible by 8.", "rsaKeyBitSize");
+            if (aesKeyBitSize % 8 != 0)
+                throw new ArgumentException("AES bit Size must be divisible by 8.", "aesKeyBitSize");
+
+            _aes = Aes.Create();
+            _aes.Mode = CipherMode.CBC;
+            _aes.KeySize = aesKeyBitSize;
+            _aes.BlockSize = blockSize;
+            _aes.GenerateKey();
+            _aes.GenerateIV();
+
+            _rsa = new RSACryptoServiceProvider(rsaKeyBitSize);
+        }
+
+        public byte[] Encrypt(string inputString) => Encrypt(Encoding.UTF8.GetBytes(inputString));
+
+        public byte[] Encrypt(byte[] inputBuffer)
+        {
+            using (var encryptor = _aes.CreateEncryptor())
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputBuffer, 0, inputBuffer.Length);
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+        
+        public string DecryptToString(byte[] inputBuffer) => Encoding.UTF8.GetString(Decrypt(inputBuffer));
+
+        public byte[] Decrypt(byte[] inputBuffer)
+        {
+            using (var decryptor = _aes.CreateDecryptor())
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputBuffer, 0, inputBuffer.Length);
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
